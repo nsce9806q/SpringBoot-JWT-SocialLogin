f7765f04c0943cf97ca7d75e3dc0b99f1cfe5872
ChangEn Yea <nsce9806q@gmail.com>
2022-10-05 15:43:37 +0900

    First Commit

---
+++ src/main/java/study/jwtsociallogin/JwtSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package study.jwtsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSocialLoginApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtSocialLoginApplication.class, args);
+  }
+
+}
---
+++ src/test/java/study/jwtsociallogin/JwtSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jwtsociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSocialLoginApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
